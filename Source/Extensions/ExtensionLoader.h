6c348f234f8714f79de04a8d4627ae9348d0c27c
Convery <Convery10122@gmail.com>
2016-03-06 19:34:08 +0100

    Extension headers.

---
+++ Source/Extensions/ExtensionLoader.h
@@ -0,0 +1,18 @@
+/*
+    Initial author: (https://github.com/)Convery
+    License: LGPL 3.0
+    Started: 2016-3-6
+    Notes:
+        1. Enumerate extension files.
+        2. Load them into process memory.
+        3. Call onInitialisationStart for each.
+*/
+
+#pragma once
+
+// Load and unload the extensions.
+void LoadExtensions();
+void UnloadExtensions();
+
+// Exported post initialization caller.
+extern "C" void FinalizeExtensions();
---
+++ Source/Extensions/Interface.h
@@ -0,0 +1,19 @@
+/*
+    Initial author: (https://github.com/)Convery
+    License: LGPL 3.0
+    Started: 2016-3-6
+    Notes:
+        Extension data-representation.
+*/
+
+#pragma once
+#include <stdint.h>
+
+struct IExtension
+{
+    char Name[32]{};
+    void(__cdecl *onExtensionUnloading)(void);
+    void(__cdecl *onInitializationStart)(void);
+    void(__cdecl *onInitializationComplete)(void);
+    void(__cdecl *onMessage)(uint32_t Message, ...);
+};
